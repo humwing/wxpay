62d0df6c7604bcc05a7f04d036b1b6f3e592a807
hmw <hmw@eebbk.com>
2023-10-26 14:14:43 +0800

    fix: 导出支持commonjs提示

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 
+export {default as WXPayV2} from './wxpay-v2.js'
+export {default as WXPayV3} from './wxpay-v3.js'
+export {default as payUtils} from './payUtils.js'
+export * as util from './util.js'
+
 import WXPayV2 from './wxpay-v2.js'
 import WXPayV3 from './wxpay-v3.js'
 import payUtils from './payUtils.js'
